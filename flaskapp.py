1b23aab0db5cc8257cceccc86d3f721410a5f8ce
Gfreshstyle <giovanniespina@yahoo.com>
2016-10-19 18:02:38 +0800

    My Flask said Hello to You

--- flaskapp.py
+++ flaskapp.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def hello_world():
     return 'hello flask'
 
+@app.route('/greet')
+def greet(name='name'):
+	return 'hello ' +name 
+
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
