0a2b51b0e0675945a361e5573c729d3cf169e176
Gfreshstyle <giovanniespina@yahoo.com>
2016-10-19 18:02:12 +0800

    My Flask

--- flaskapp.py
+++ flaskapp.py
@@ -1,14 +1,5 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'hello flask'
-
-@app.route('/greet')
-def greet(name='name'):
-	return 'hello ' +name 
-
-
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
